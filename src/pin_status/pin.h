61e678e0645604250cd7a04cc2fe7f11fa1d500d
krmnino <kurt.manrique.n@gmail.com>
2020-10-07 19:05:28 -0400

    removed old code and moved pinarray away

--- src/pin_status/pin.h
+++ src/pin_status/pin.h
@@ -5,12 +5,6 @@ typedef struct Pin{
     double duty_cycle;  
 } Pin;
 
-void print(Pin* pin){
+void print_pin(Pin* pin){
     printf("Pin ID: %d\nDuty Cycle: %f\n", pin->pin_id, pin->duty_cycle);   
-}
-
-void print_array(Pin arr[], size_t n){
-    for(int i = 0; i < n; i++){
-        print(&arr[i]);
-    }
 }
