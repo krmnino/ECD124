a61c256e05e5efaf6f10a71af32b8711abb8d77b
krmnino <kurt.manrique.n@gmail.com>
2020-10-18 00:43:23 -0400

    implemented duty cycle randomizer to test sorting

--- src/pin_status/DebugUtil.h
+++ src/pin_status/DebugUtil.h
@@ -2,17 +2,29 @@
 #define PINARRDEB
 
 #include <stdio.h>
+#include <time.h>
 #include "Pin.h"
 #include "PinArray.h"
 
 void print_pin(Pin* pin){
     printf("Pin ID: %d\nDuty Cycle: %d\nToggle: %d\n", pin->pin_id, pin->duty_cycle, pin->toggle);   
+    return;
 }
 
 void print_arr(PinArray* pin_array){
     for(int i = 0; i < pin_array->size; i++){
         print_pin(&pin_array->arr[i]);
     }
+    return;
+}
+
+void randomize_duty_cycles(PinArray* pin_array, int lower_range, int upper_range){
+    srand(time(0));
+    for(int i = 0; i < pin_array->size; i++){
+        int rand_num = rand() % (upper_range - lower_range + 1) + lower_range;
+        pin_array->arr[i].duty_cycle = rand_num;
+    }
+    return;
 }
 
 int pinid_reg(PinArray* pin_array, int index){
--- src/pin_status/Main.c
+++ src/pin_status/Main.c
@@ -8,6 +8,7 @@ const int NUMBER_OF_PINS = 48;
 
 int main(){
     PinArray* pin_array = init_arr(NUMBER_OF_PINS);
+    randomize_duty_cycles(pin_array, 1, 1000);
     sort_duty_cycles_QS(pin_array, 0, pin_array->size - 1);
     print_arr(pin_array);
     //print_pin(&pin_array->arr[30]);
