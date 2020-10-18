3e54be2059d9abcd2e2b4c7a71fb89f381a1ff24
krmnino <kurt.manrique.n@gmail.com>
2020-10-17 23:14:27 -0400

    added toggle variable to Pin struct

--- src/pin_status/Pin.h
+++ src/pin_status/Pin.h
@@ -5,18 +5,21 @@
 
 typedef struct Pin{
     int pin_id;
-    double duty_cycle;  
+    int duty_cycle;
+    int toggle;
 } Pin;
 
-void init_pin(Pin* pin, int pin_id_, double duty_cycle_){
+void init_pin(Pin* pin, int pin_id_, double duty_cycle_, int toggle_){
     pin->pin_id = pin_id_;
     pin->duty_cycle = duty_cycle_;
+    pin->toggle = toggle_
 }
 
-Pin* create_pin(int pin_id_, double duty_cycle_){
+Pin* create_pin(int pin_id_, int duty_cycle_, int toggle_){
     Pin* pin = (Pin*)malloc(sizeof(Pin));
     pin->pin_id = pin_id_;
     pin->duty_cycle = duty_cycle_;
+    pin->toggle = toggle_;
     return pin;
 }
 
