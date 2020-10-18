0a17ca8eed3746125a326b556b5f4604431d11fb
krmnino <kurt.manrique.n@gmail.com>
2020-10-17 23:53:55 -0400

    added functions to retrieve fields from Pin to ease debugging in CCS

--- src/pin_status/DebugUtil.h
+++ src/pin_status/DebugUtil.h
@@ -6,13 +6,28 @@
 #include "PinArray.h"
 
 void print_pin(Pin* pin){
-    printf("Pin ID: %d\nDuty Cycle: %f\n", pin->pin_id, pin->duty_cycle);   
+    printf("Pin ID: %d\nDuty Cycle: %d\nToggle: %d\n", pin->pin_id, pin->duty_cycle, pin->toggle);   
 }
 
 void print_arr(PinArray* pin_array){
     for(int i = 0; i < pin_array->size; i++){
         print_pin(&pin_array->arr[i]);
     }
+}
+
+int pinid_reg(PinArray* pin_array, int index){
+    int pin_id = pin_array->arr[index].pin_id;
+    return pin_id;
+}
+
+int dutycycle_reg(PinArray* pin_array, int index){
+    int duty_cycle = pin_array->arr[index].duty_cycle;
+    return duty_cycle;
+}
+
+int toggle_reg(PinArray* pin_array, int index){
+    int toggle = pin_array->arr[index].toggle;
+    return toggle;
 }
 
 #endif
