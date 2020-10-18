1c3f9a30e27473b9d64e964ec2d6d4b6ba108cb8
krmnino <kurt.manrique.n@gmail.com>
2020-10-18 00:25:06 -0400

    implemented quick sort algorithm

--- src/pin_status/PinArray.h
+++ src/pin_status/PinArray.h
@@ -2,6 +2,8 @@
 #include "Pin.h"
 #endif
 
+#include <stdio.h>
+
 #ifndef PINARR
 #define PINARR
 
@@ -19,7 +21,7 @@ PinArray* init_arr(size_t size_){
     }
     int i;
     for(i = 0; i < size_; i++){
-        init_pin(&pin_array->arr[i], i, 0, 0);
+        init_pin(&pin_array->arr[i], i, 48-i, i % 2);
     }
     return pin_array;
 }
@@ -32,7 +34,7 @@ void swap(Pin* p1, Pin* p2){
 }
 
 //Sort pins by duty cycles using insertion sort
-void sort_duty_cycles(PinArray* pin_array){
+void sort_duty_cycles_IS(PinArray* pin_array){
     int i;
     for(i = 0; i < pin_array->size; i++){
         int j;
@@ -43,6 +45,32 @@ void sort_duty_cycles(PinArray* pin_array){
         }
     }
     return;
+}
+
+//Sort pins by duty cycles using quick sort
+int get_partition(Pin arr[], int start, int end){
+    Pin pivot = arr[end];
+    int i = (start - 1);
+    int j;
+    for(j = start; j <= end - 1; j++){
+        if(arr[j].duty_cycle < pivot.duty_cycle){
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[end]);
+    return (i + 1);
+}
+
+void sort_duty_cycles_QS(PinArray* pin_array, int start, int end){
+    if(start >= end){
+        return;
+    }
+    else{
+        int partition = get_partition(pin_array->arr, start, end);
+        sort_duty_cycles_QS(pin_array, start, partition - 1);
+        sort_duty_cycles_QS(pin_array, partition + 1, end);
+    }
 }
 
 #endif
