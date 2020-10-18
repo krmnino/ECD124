493430fc1f7dbbb2a8ca2081bc658ee73035f380
krmnino <kurt.manrique.n@gmail.com>
2020-10-18 00:30:52 -0400

    testing quick sort

--- src/pin_status/Main.c
+++ src/pin_status/Main.c
@@ -8,7 +8,7 @@ const int NUMBER_OF_PINS = 48;
 
 int main(){
     PinArray* pin_array = init_arr(NUMBER_OF_PINS);
-    sort_duty_cycles(pin_array);
+    sort_duty_cycles_QS(pin_array, 0, pin_array->size - 1);
     print_arr(pin_array);
-    print_pin(&pin_array->arr[30]);
+    //print_pin(&pin_array->arr[30]);
 }
