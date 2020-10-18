ec41a215a75e704de83f1d2428cef48f49d2725a
krmnino <kurt.manrique.n@gmail.com>
2020-10-17 23:15:15 -0400

    typo: missing semicolon

--- src/pin_status/Pin.h
+++ src/pin_status/Pin.h
@@ -12,7 +12,7 @@ typedef struct Pin{
 void init_pin(Pin* pin, int pin_id_, double duty_cycle_, int toggle_){
     pin->pin_id = pin_id_;
     pin->duty_cycle = duty_cycle_;
-    pin->toggle = toggle_
+    pin->toggle = toggle_;
 }
 
 Pin* create_pin(int pin_id_, int duty_cycle_, int toggle_){
