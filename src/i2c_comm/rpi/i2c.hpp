46bf3c08f5d9e745f4a7d5890ce6b122b9b44715
krmnino <kurt.manrique.n@gmail.com>
2020-11-17 15:51:25 -0500

    finished i2c implementation for RPi

--- src/i2c_comm/rpi/i2c.hpp
+++ src/i2c_comm/rpi/i2c.hpp
@@ -3,47 +3,68 @@
 #include <stdint.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <cstring>
+
+#include <stdio.h>
 
 class Session{
 private:
-public:
     int fd;
-    uint8_t curr_slave;
-    uint8_t msg_received;
-    uint8_t msg_sent;
+    uint8_t addr;
+public:
+    char msg_received[256] = {0};
 
     Session();
     ~Session();
-    int init_i2c();
-    int read_from(uint8_t, uint8_t);
-    int write_to(uint8_t);
+    int init_i2c(uint8_t);
+    int get_file_descriptor();
+    uint8_t get_address();
+    int request_i2c();
+    int send_i2c(const char* msg);
 };
 
 Session::Session(){}
 
-Session::~Session(){}
+Session::~Session(){
+    if(this->fd){
+        close(fd);
+    }
+}
 
-int Session::init_i2c(){
-    this->fd = open("/dev/i2c-1", O_RDWR);
+int Session::get_file_descriptor(){
     return this->fd;
-} 
+}
 
-int Session::read_from(uint8_t addr, uint8_t data){
-    if(ioctl(this->fd, I2C_SLAVE, addr) < 0){
+uint8_t Session::get_address(){
+    return this->addr;
+}
+
+int Session::init_i2c(uint8_t addr){
+    this->fd = open("/dev/i2c-1", O_RDWR);
+    if(this->fd < 0){
         return -1;
     }
-    if(read(fd, &this->msg_received, 1) != 1){
+    this->addr = addr;
+    if(ioctl(this->fd, I2C_SLAVE, this->addr) < 0){
+        return -1;
+    }
+    return 0;
+} 
+
+int Session::request_i2c(){
+    if(read(this->fd, &this->msg_received, 256) != 1){
         return -1;
     }
     return 0;
 }
 
-int Session::write_to(uint8_t addr){
-    if(ioctl(this->fd, I2C_SLAVE, addr) < 0){
-        return -1;
-    }
-    if(write(fd, &this->msg_sent, 2) != 1){
-        return -1;
+int Session::send_i2c(const char* msg){
+    int msg_char = 0;
+    for(unsigned int i = 0; i < strlen(msg); i++){
+        msg_char = msg[i];
+	if(write(this->fd, &msg_char, 2) < 0){
+	    return -1;
+        }
     }
     return 0;
 }
--- src/i2c_comm/rpi/main.cpp
+++ src/i2c_comm/rpi/main.cpp
@@ -1,11 +1,26 @@
 #include "i2c.hpp"
-#include <string>
+#include <stdio.h>
 
 int main(){
-    Session i2c_session;
-    std::string msg = "example";
-    for(unsigned int i = 0; i < msg.size(); i++){
-        i2c_session.msg_sent = msg[i];
-        i2c_session.write_to(0x2);
+    Session green_session;
+    Session red_session;
+
+    int ret = green_session.init_i2c(0x2);
+    if(ret < 0){
+	printf("Couldn't init i2c");
     }
+
+    int ret2 = red_session.init_i2c(0x3);
+    if(ret2 < 0){
+	printf("Couldn't init i2c");
+    }
+
+    int ret3 = green_session.send_i2c("example");
+    
+
+    green_session.request_i2c();
+    printf("Green: %s\n", green_session.msg_received);
+
+    red_session.request_i2c();
+    printf("Red: %s\n", red_session.msg_received);
 }
