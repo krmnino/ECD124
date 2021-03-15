c02462f3337aea34ea1b43f06624c03e4dc9427c
krmnino <kurt.manrique.n@gmail.com>
2021-03-15 03:15:58 -0400

    created Table class to facilitate extracting columns

--- src/export_data/CSV_Parser.py
+++ src/export_data/CSV_Parser.py
@@ -1,28 +1,35 @@
 import numpy as np
 
-def parse_file(path):
-    header = True
-    header_index = {}
-    contents = {}
-    with open(path) as file:
-        for line in file:
-            if header:
-                header_split = line.split(',')
-                for i, elem in enumerate(header_split):
-                    header_index[i] = elem
-                header = False
-                continue
-            line_split = line.split(',')
-            for i, elem in enumerate(line_split):
-                try:
-                    convert_elem = float(elem)
-                except:
-                    convert_elem = elem
-                    pass
-                if header_index[i] not in contents.keys():
-                    contents[header_index[i]] = np.array([convert_elem])
-                else:
-                    contents[header_index[i]] = np.append(contents[header_index[i]], convert_elem)
-    return contents
+class Table:
+    def __init__(self, path):
+        self.filename = path
+        self.header_index = {}
+        self.contents = {}
+        header = True
+        with open(self.filename) as file:
+            for line in file:
+                if header:
+                    header_split = line.split(',')
+                    for i, elem in enumerate(header_split):
+                        self.header_index[i] = elem.replace('\n', '')
+                    header = False
+                    continue
+                line_split = line.split(',')
+                for i, elem in enumerate(line_split):
+                    try:
+                        convert_elem = float(elem)
+                    except:
+                        convert_elem = elem
+                        pass
+                    if self.header_index[i] not in self.contents.keys():
+                        self.contents[self.header_index[i]] = np.array([convert_elem])
+                    else:
+                        self.contents[self.header_index[i]] = np.append(self.contents[self.header_index[i]], convert_elem)
 
-parse_file('data/test.csv')
+    def get_column(self, field):
+        return self.contents[field]
+    
+    def update_data(self):
+        for line in reversed(list(open(self.filename))):
+            print(line.rstrip())
+    
